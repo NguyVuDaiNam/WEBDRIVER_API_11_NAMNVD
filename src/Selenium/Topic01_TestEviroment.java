3d9d8c84c67f657b76ad064056df2b010c12b186
Administrator <Administrator@NSIGNRDJ3NZRKQR>
2019-06-22 17:03:18 +0700

    Init Repository

---
+++ src/Selenium/Topic01_TestEviroment.java
@@ -0,0 +1,49 @@
+package Selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic01_TestEviroment {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {	
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/v4/");
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+		// Login Page Url matching
+		String loginPageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	}
+
+	@Test
+	public void TC_02_ValidatePageTitle() {
+		// Login Page title
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+	}
+
+	@Test
+	public void TC_03_LoginFormDisplayed() {
+		// Login form displayed
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
